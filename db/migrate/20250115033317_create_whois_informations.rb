248f573155a4665d463785b40ca87acaaf9aece0
Laura Santiago Campos Oliveira <laaaaaaa45@gmail.com>
2025-01-21 20:18:15 -0300

    Create basic whois information check

---
+++ db/migrate/20250115033317_create_whois_informations.rb
@@ -0,0 +1,19 @@
+class CreateWhoisInformations < ActiveRecord::Migration[8.0]
+  def change
+    create_table :whois_informations do |t|
+      t.string :owner_name
+      t.string :owner_email
+      t.date :registered_at
+      t.string :iana_id
+      t.string :owner_phone_number
+      t.date :last_updated_at
+      t.date :expires_at
+      t.string :name_servers, default: [], array: true
+      t.string :whois_server, default: [], array: true
+      t.string :domain_statuses
+      t.references :domain, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
